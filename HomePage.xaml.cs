4efb8231746f0057fe2470be15b657836efbc366
M00nl1ght07 <denis72728282@yandex.ru>
2025-02-24 01:13:14 +0300

    добавил номера и редирект на номера отелей

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -33,22 +33,18 @@ namespace BookingWPF
         {
             try
             {
-                using (SqlDataReader reader = DatabaseConnection.ExecuteReader(@"
-            SELECT h.HotelID, h.Name, h.City, h.Rating, h.BasePrice, h.PhotoPath, h.Description,
-                   (SELECT COUNT(*) FROM Rooms r WHERE r.HotelID = h.HotelID) as RoomCount
-            FROM Hotels h
-            WHERE h.IsPopular = 1"))
+                using (SqlDataReader reader = DatabaseConnection.ExecuteReader(
+                    "SELECT HotelID, Name, City, BasePrice, Description, PhotoPath FROM Hotels WHERE IsPopular = 1"))
                 {
                     PopularHotelsPanel.Children.Clear();
 
                     while (reader.Read())
                     {
-                        // Создаем карточку отеля
                         Border hotelCard = new Border
                         {
                             Width = 300,
-                            Height = 350,
-                            Margin = new Thickness(0, 0, 20, 0),
+                            Height = 400,
+                            Margin = new Thickness(0, 0, 20, 20),
                             Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2D2D2D")),
                             CornerRadius = new CornerRadius(10)
                         };
@@ -103,19 +99,19 @@ namespace BookingWPF
                             Content = "Подробнее",
                             Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#007ACC")),
                             Foreground = Brushes.White,
-                            Padding = new Thickness(20, 5, 20, 5)
+                            Padding = new Thickness(20, 10, 20, 10),
+                            Margin = new Thickness(0),
+                            BorderThickness = default(Thickness)
                         };
+
+                        int hotelId = Convert.ToInt32(reader["HotelID"]);
+                        detailsButton.Click += (s, e) => NavigationService.Navigate(new RoomSelectionPage(hotelId));
 
                         infoPanel.Children.Add(detailsButton);
 
                         grid.Children.Add(hotelImage);
                         grid.Children.Add(infoPanel);
                         hotelCard.Child = grid;
-
-                        // Добавляем обработчик клика
-                        int hotelId = Convert.ToInt32(reader["HotelID"]);
-                        detailsButton.Click += (s, e) => OpenHotelDetails(hotelId);
-                        hotelCard.Cursor = Cursors.Hand;
 
                         PopularHotelsPanel.Children.Add(hotelCard);
                     }
@@ -126,12 +122,6 @@ namespace BookingWPF
                 MessageBox.Show($"Ошибка при загрузке популярных отелей: {ex.Message}", 
                     "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-        }
-
-        private void OpenHotelDetails(int hotelId)
-        {
-            // Здесь будет переход на страницу отеля
-            // NavigationService.Navigate(new HotelDetailsPage(hotelId));
         }
 
         private void SpecialOffer_Click(object sender, RoutedEventArgs e)
